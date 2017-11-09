5bf0f1735f2443bdd56b6b2f7004a75e65bf3574
Robert Torzilli <torzillirobert@gmail.com>
2017-11-08 20:10:00 -0500

    Added in creating a dictionary from a txt file

--- PythonCode/inputEditor.py
+++ PythonCode/inputEditor.py
@@ -8,13 +8,50 @@ import os.path as osP
 
 # Windows relative path
 currentDir = osP.abspath(osP.dirname(__file__))
-inputFile = osP.join(currentDir, "..\Model\TestPythonReplace.txt")
-outputFile = r'C:\Users\Robert\Documents\School\NENG685\GeDetector_Project\Model\TestPython.txt'
 
-replacements = {'xHole':'5', 'yHole':'7778', 'zHole':'0', 'rHole':'4555'}
+# This should be the model of the MCNP 
+mcnpModelBase = osP.join(currentDir, "..\Model\TestPythonReplace.txt")
 
-with open(inputFile) as infile, open(outputFile, 'w') as outfile:
-    for line in infile:
-        for src, target in replacements.items():
-            line = line.replace(src, target)
-        outfile.write(line)
+# Needs to be renamed 
+outputFile = osP.join(currentDir, "..\Model\TestEditFile.txt")
+outputFile2 = osP.join(currentDir, "..\Model\TestEditDict.txt")
+
+# =============================================================================
+# Dimensions needs to be columned
+# The First column must match the variable place holder in the model exactly
+# The Second column is the value
+# =============================================================================
+dimensionsFile = osP.join(currentDir, "..\Model\holeDimensions.txt")
+
+holeDims = {'xHole':'5', 'yHole':'7778', 'zHole':'0', 'rHole':'4555'}
+
+# Opens file and replaces terms within that are matched in a defined dictionary
+def editFile (replace, inputFile, outputFile):
+    with open(inputFile) as infile, open(outputFile, 'w') as outfile:
+        for line in infile:
+            for src, target in replace.items():
+                line = line.replace(src, target)
+            outfile.write(line)
+    return print("Success")
+
+# Changes a given dictionary key  value. Purpose is to automate optimization
+def editDictionary (inputDict, key_replace, replaceValue):
+    for key in inputDict.keys():
+        if key == key_replace:
+            inputDict[key] = replaceValue
+    return print("Values Changed for " + str(key_replace)+" key " + str(replaceValue))
+    
+def createDictionary (dimensionsFile):
+    with open(dimensionsFile) as inDims:
+        dictionary = {}
+        for line in inDims:
+            keyPass = line.split()
+            dictionary[keyPass[0]] = keyPass[1]
+    return dictionary
+
+dimmy = createDictionary(dimensionsFile)
+
+editFile(dimmy,mcnpModelBase,outputFile)
+
+editDictionary(holeDims,'zHole', '789')
+editFile(holeDims,mcnpModelBase,outputFile2)
