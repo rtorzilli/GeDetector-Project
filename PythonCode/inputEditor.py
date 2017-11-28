d32235cb7e2b61f2edcf10b55aed564d7c6063a1
Robert Torzilli <torzillirobert@gmail.com>
2017-11-27 23:47:53 -0500

    attempted to fix gitignore, takes 5 mins to go through a single iteration of each parameter for each position, not saving variable inputs anymore since its in the output file

--- PythonCode/inputEditor.py
+++ PythonCode/inputEditor.py
@@ -5,19 +5,19 @@ Created on Wed Nov  8 18:34:19 2017
 @author: Robert Torzilli
 
 To run as intended the user needs to provide the following:
-    A MCNP base input deck with variables to adjust
-    A txt document dictating what each variable name in the MCNP model is
-    
+	A MCNP base input deck with variables to adjust
+	A txt document dictating what each variable name in the MCNP model is
+	
 This program will output the following:
-    A runnable MCNP input file to be used with MCNP
-    An altered txt file for the variables for the purpose of optimizing
-    
+	A runnable MCNP input file to be used with MCNP
+	An altered txt file for the variables for the purpose of optimizing
+	
 This script defines methods first since it may have to use a few while creating 
 and reading file names in future iterations.
 
 This script has many fail points currently, use with caution
-    For Stability the keywords of the Variable File have been hard coded
-    since dictionaries are not ordered
+	For Stability the keywords of the Variable File have been hard coded
+	since dictionaries are not ordered
 """
 import os
 import subprocess as subP
@@ -27,19 +27,6 @@ import numpy as np
 mcnpModel = "\HPGe_Generic_Model.i"
 variablesToAdjust = "\Variable_Input.txt"
 
-# ORDER IS VERY IMPORTANT FOR THESE FOUR LISTS
-compareValues = ["\Pos_1_experimental.txt","\Pos_2_experimental.txt",
-                 "\Pos_3_experimental.txt","\Pos_5_experimental.txt"]
-#Names of files to merge
-fileNames = ["\HPGe_Generic_Model.i","\Pos_1_0cm_centered.i",
-             "\Pos_2_0cm_flushed.i","\Pos_3_7cm_centered.i","\Pos_5_offset.i"]
-currentPositionFolder = ['\Position1','\Position2','\Position3','\Position5']
-# Data Summary Output Locations
-dataOutLoc = [batLocation+ '\MCNP_Output\Position1\Data_and_RelativeErr\Data_Pos1_',
-              batLocation+ '\MCNP_Output\Position2\Data_and_RelativeErr\Data_Pos2_',
-              batLocation+ '\MCNP_Output\Position3\Data_and_RelativeErr\Data_Pos3_',
-              batLocation+ '\MCNP_Output\Position5\Data_and_RelativeErr\Data_Pos5_']
-
 # Name of the batch file to run
 mcnpBat = "runMCNP.bat"
 
@@ -47,10 +34,27 @@ mcnpBat = "runMCNP.bat"
 currentDir = os.path.abspath(os.path.dirname(__file__))
 
 # Windows path to runMCNP.bat/parent directory
-batLocation = os.path.split(currentDir)[0]
+parentDir = os.path.split(currentDir)[0]
+
+# ORDER IS VERY IMPORTANT FOR THESE FOUR LISTS
+# Make sure there are no empty lines at the end of these files or an index error will occur
+compareValues = ["\Pos_1_experimental.txt","\Pos_2_experimental.txt",
+				 "\Pos_3_experimental.txt","\Pos_5_experimental.txt"]
+#Names of files to merge
+fileNames = ["\HPGe_Generic_Model.i","\Pos_1_0cm_centered.i",
+			 "\Pos_2_0cm_flushed.i","\Pos_3_7cm_centered.i","\Pos_5_offset.i"]
+currentPositionFolder = ['\Position1','\Position2','\Position3','\Position5']
+# Data Summary Output Locations
+dataOutLoc = [parentDir+ '\MCNP_Output\Position1\Data_and_RelativeErr\Data_Pos1_',
+			  parentDir+ '\MCNP_Output\Position2\Data_and_RelativeErr\Data_Pos2_',
+			  parentDir+ '\MCNP_Output\Position3\Data_and_RelativeErr\Data_Pos3_',
+			  parentDir+ '\MCNP_Output\Position5\Data_and_RelativeErr\Data_Pos5_']
+
+# Where Recorded Variable output will be placed (not nessearcily the best one)
+varOut = parentDir+ '\MCNP_Output\LastVariableInput\Variable_Input'
 
 #User Defined Output file -- Change the runMCNP bat as well, daughter is in the Parent loop below
-mcnpOut=batLocation+'\MCNP_Output\HPGe_Output_Model'
+mcnpOut=parentDir+'\MCNP_Output\HPGe_Output_Model'
 
 # =============================================================================
 # Dimensions needs to be columned
@@ -71,70 +75,70 @@ outputFile = os.path.join(currentDir, "..\Output" + mcnpModel)
 
 # Opens file and replaces terms within that are matched in a defined dictionary
 def editFile (replace, inputFile, outputFile):
-    with open(inputFile) as infile, open(outputFile, 'w') as outfile:
-        for line in infile:
-            for src, target in replace.items():
-                line = line.replace(src, target)
-            outfile.write(line)
-    return
+	with open(inputFile) as infile, open(outputFile, 'w') as outfile:
+		for line in infile:
+			for src, target in replace.items():
+				line = line.replace(src, target)
+			outfile.write(line)
+	return
 def mergeFile(baseFile, mergeFile,output):
-    filenames = [baseFile, mergeFile]
-    with open(output, 'w') as outfile:
-        for fname in filenames:
-            with open(fname) as infile:
-                for line in infile:
-                    outfile.write(line)           
+	filenames = [baseFile, mergeFile]
+	with open(output, 'w') as outfile:
+		for fname in filenames:
+			with open(fname) as infile:
+				for line in infile:
+					outfile.write(line)           
 
 #Create a file with data, rel err, and the average error
 def createFile(dataDict,relErrDict,avgErr,ouput):
-    with open(ouput,'w') as outfile:
-        outfile.write('Energy Bin    Value   Uncertainity\n')
-        for key,value in dataDict.items():
-            outfile.write("{0} {1} {2}\n".format(str(key), str(value[0]),
-                          str(value[1]))) 
-        outfile.write("\nEnergy Bin   Relative Error\n")
-        for key,value in relErrDict.items():
-            outfile.write("{0} {1}\n".format(str(key), str(value))) 
-        outfile.write("\nAverage Error\n")
-        outfile.write(str(avgErr))
-    outfile.close()
+	with open(ouput,'w') as outfile:
+		outfile.write('Energy Bin    Value   Uncertainity\n')
+		for key,value in dataDict.items():
+			outfile.write("{0} {1} {2}\n".format(str(key), str(value[0]),
+						  str(value[1]))) 
+		outfile.write("\nEnergy Bin   Relative Error\n")
+		for key,value in relErrDict.items():
+			outfile.write("{0} {1}\n".format(str(key), str(value))) 
+		outfile.write("\nAverage Error\n")
+		outfile.write(str(avgErr))
+	outfile.close()
 # Creates a dictionary from a text file    
 def createDictionary (dimensionsFile):
-    with open(dimensionsFile) as inDims:
-        dictionary = {}
-        for line in inDims:
-            keyPass = line.split()
-            dictionary[keyPass[0]] = keyPass[1]
-    return dictionary
+	with open(dimensionsFile) as inDims:
+		dictionary = {}
+		for line in inDims:
+			keyPass = line.split()
+			dictionary[keyPass[0]] = keyPass[1]
+	return dictionary
 
 # Search the data output file for a specfic keyword and copy taht line as a dict
 def getData(dataFile,wantedValues):
-    with open(dataFile) as inputFile:
-        dictionary ={}
-        # Read in each line
-        for line in inputFile:
-            #Make sure its not a blank line
-            if line.strip():
-                for key in wantedValues.keys():
-                    # does the first value equal a key?
-                    if line.split(maxsplit=1)[0] == key:    
-                        # Grab Energy Bin, Efficency, AND uncertainty col 3
-                        keyPass = line.split()
-                        dictionary[keyPass[0]] = [keyPass[1],keyPass[2]]
-    return dictionary
+	with open(dataFile) as inputFile:
+		dictionary ={}
+		# Read in each line
+		for line in inputFile:
+			#Make sure its not a blank line
+			if line.strip():
+				for key in wantedValues.keys():
+					# does the first value equal a key?
+					if line.split(maxsplit=1)[0] == key:    
+						# Grab Energy Bin, Efficency, AND uncertainty col 3
+						keyPass = line.split()
+						dictionary[keyPass[0]] = [keyPass[1],keyPass[2]]
+	return dictionary
 
 # =============================================================================
 # Compare data using relative error (the times 12 is due to the format for the mcnp
 # out values). 
 # =============================================================================
 def relativeErr(experimentalData,outputData):
-    errorDict={}
-    for key in experimentalData.keys():
-        expData = float(experimentalData[key])
-        modelData = float(outputData[key][0])
-        errOfBin=np.abs(expData-12*modelData)/expData
-        errorDict[key]=errOfBin
-    return errorDict
+	errorDict={}
+	for key in experimentalData.keys():
+		expData = float(experimentalData[key])
+		modelData = float(outputData[key][0])
+		errOfBin=np.abs(expData-12*modelData)/expData
+		errorDict[key]=errOfBin
+	return errorDict
 
 # =============================================================================
 #  Once fleshed out this function will pull in the dictionary keys and write
@@ -142,32 +146,36 @@ def relativeErr(experimentalData,outputData):
 #  Either through numerical analysis or from a for loop iterating through
 # =============================================================================
 def recreateDimFile(valuesToChange,newValue,fileIn):
-    with open(fileIn, "w")as text_file:
-        print(valuesToChange[0] + " "+ str(newValue[0]) + 
-              "\n" + valuesToChange[1] + " " + str(newValue[1]) +
-              "\n" + valuesToChange[2] + " " + str(newValue[2]) +
-              "\n" + valuesToChange[3] + " " + str(newValue[3]) 
-              , file=text_file)
+	with open(fileIn, "w")as text_file:
+		print(valuesToChange[0] + " "+ str(newValue[0]) + 
+			  "\n" + valuesToChange[1] + " " + str(newValue[1]) +
+			  "\n" + valuesToChange[2] + " " + str(newValue[2]) +
+			  "\n" + valuesToChange[3] + " " + str(newValue[3]) 
+			  , file=text_file)
  
 # =============================================================================
 #     Determined Iteration Range
 # =============================================================================
-TDL=100
+TDL=1
+topDLDefault = 12.95899333
 topDeadLayerMin = 11.5
 topDeadLayerMax = 13.07599333
 topDeadLayer=np.linspace(topDeadLayerMin,topDeadLayerMax,TDL)
 
-GL = 25
+GL = 1
+geDefault = 4.63899333 
 geLengthMin = 3.79399133
 geLengthMax = 5.483999333
 geLength= np.linspace(geLengthMin,geLengthMax,GL)
 
-CR=15
+CR=1
+sideDLDefault = 4.02
 sideDeadLayerMin = 3.92
 sideDeadLayerMax = 4.12
 sideDeadLayer = np.linspace(sideDeadLayerMin,sideDeadLayerMax,CR)
 
-n=5
+n=1
+geDensityDefault = -5.32
 geDensityMin = -5.32
 geDensityMax = -5.35
 geDensity = np.linspace(geDensityMin,geDensityMax,n)
@@ -183,7 +191,11 @@ for posSource in fileNames[1:]:
 							  + compareValues[currPos])
 	energyBins = createDictionary(energyFile)
 	
-	mcnpOutRename = batLocation+'\MCNP_Output' + currentPositionFolder[currPos] + '\HPGe_Output_Model_'
+	mcnpOutRename = parentDir+'\MCNP_Output' + currentPositionFolder[currPos] + '\HPGe_Output_Model_'
+	
+	newDimensionValues = [geDensityDefault,geDefault,topDLDefault,
+					  sideDLDefault]
+	recreateDimFile(dimensionKeys,newDimensionValues,dimensionsFile)
 	
 	# This merges the source to variable model with the source model
 	# Files that will be merged to create base model currently just the base 
@@ -215,7 +227,7 @@ for posSource in fileNames[1:]:
 
 		# 2 Run "MCNP" currently a test bat file
 		# Batch file that will be called
-		runBat = subP.Popen(mcnpBat, cwd=batLocation, shell=True)
+		runBat = subP.Popen(mcnpBat, cwd=parentDir, shell=True)
 		runBat.communicate()
 		runBat.wait()
 
@@ -250,15 +262,15 @@ for posSource in fileNames[1:]:
 
 		# 5 Repeat/Clean Up/Record
 		# Record Values
-		dataOut = dataOutLoc[currPos]+'TopDeadLayer_'+'.txt'
-		createFile(freshData,relError,averageError,dataOut+str(i))
+		dataOut = dataOutLoc[currPos]+'TopDeadLayer_'
+		createFile(freshData,relError,averageError,dataOut+str(i)+'.txt')
 
 		# Check first if file exists if it does rename it
 		if (os.path.isfile(mcnpOut)):
 			os.rename(mcnpOut,mcnpOutRename+"TopDeadLayer_"+str(i)+'.txt')
 	
 	#Reset for next parameter
-    #Alter Dim File to best found 
+	#Alter Dim File to best found 
 	newDimensionValues = [geDensity[0],geLength[0],bestTopDeadLayer,
 					  sideDeadLayer[0]]
 	recreateDimFile(dimensionKeys,newDimensionValues,dimensionsFile)
@@ -272,7 +284,7 @@ for posSource in fileNames[1:]:
 
 		# 2 Run "MCNP" currently a test bat file
 		# Batch file that will be called
-		runBat = subP.Popen(mcnpBat, cwd=batLocation, shell=True)
+		runBat = subP.Popen(mcnpBat, cwd=parentDir, shell=True)
 		runBat.communicate()
 		runBat.wait()
 
@@ -290,8 +302,8 @@ for posSource in fileNames[1:]:
 			oldAvgErr=averageError
 			bestGeLength = geLength[i]
 		# Record Values
-		dataOut = dataOutLoc[currPos]+'CrystalLength_'+'.txt'
-		createFile(freshData,relError,averageError,dataOut+str(i))
+		dataOut = dataOutLoc[currPos]+'CrystalLength_'
+		createFile(freshData,relError,averageError,dataOut+str(i)+'.txt')
 		#print("Average Error was: " +str(averageError)+"\n")
 
 		# 4 Create new Input Values (Based off predetermeind Iteration)
@@ -318,7 +330,7 @@ for posSource in fileNames[1:]:
 
 		# 2 Run "MCNP" currently a test bat file
 		# Batch file that will be called
-		runBat = subP.Popen(mcnpBat, cwd=batLocation, shell=True)
+		runBat = subP.Popen(mcnpBat, cwd=parentDir, shell=True)
 		runBat.communicate()
 		runBat.wait()
 
@@ -337,8 +349,8 @@ for posSource in fileNames[1:]:
 			bestSideDeadLayer = sideDeadLayer[i]
 			
 		# Record Values
-		dataOut = dataOutLoc[currPos]+'SideDeadLayer_'+'.txt'
-		createFile(freshData,relError,averageError,dataOut+str(i))
+		dataOut = dataOutLoc[currPos]+'SideDeadLayer_'
+		createFile(freshData,relError,averageError,dataOut+str(i)+'.txt')
 		#print("Average Error was: " +str(averageError)+"\n")
 
 		# 4 Create new Input Values (Based off predetermeind Iteration)
@@ -365,7 +377,7 @@ for posSource in fileNames[1:]:
 
 		# 2 Run "MCNP" currently a test bat file
 		# Batch file that will be called
-		runBat = subP.Popen(mcnpBat, cwd=batLocation, shell=True)
+		runBat = subP.Popen(mcnpBat, cwd=parentDir, shell=True)
 		runBat.communicate()
 		runBat.wait()
 
@@ -384,8 +396,8 @@ for posSource in fileNames[1:]:
 			bestGeDensity = geDensity[i]
 		#print("Average Error was: " +str(averageError)+"\n")
 		# Record Values
-		dataOut = dataOutLoc[currPos]+'GeDensity_'+'.txt'
-		createFile(freshData,relError,averageError,dataOut+str(i))
+		dataOut = dataOutLoc[currPos]+'GeDensity_'
+		createFile(freshData,relError,averageError,dataOut+str(i)+'.txt')
 		# 4 Create new Input Values (Based off predetermeind Iteration)
 		newDimensionValues = [geDensity[i],bestGeLength,bestTopDeadLayer,
 							  bestSideDeadLayer]
@@ -408,7 +420,7 @@ for posSource in fileNames[1:]:
 
 	# 2 Run "MCNP" currently a test bat file
 	# Batch file that will be called
-	runBat = subP.Popen(mcnpBat, cwd=batLocation, shell=True)
+	runBat = subP.Popen(mcnpBat, cwd=parentDir, shell=True)
 	runBat.communicate()
 	runBat.wait()
 
